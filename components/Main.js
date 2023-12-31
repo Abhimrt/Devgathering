8e2fd71144ec787c4826797aa32bd3f1bbc2b84b
Abhishek Singhal <abhisinghal112001@gmail.com>
2023-12-31 19:48:45 +0530

    main section completed

--- components/Main.js
+++ components/Main.js
@@ -1,35 +1,36 @@
-"use client"
+"use client";
 import React, { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import Button from "./Button";
 
 const Main = () => {
-    const [day, setDay] = useState()
-    const [hour, setHour] = useState()
-    const [minute, setMinute] = useState()
-    const [second, setSecond] = useState()
+  const [day, setDay] = useState("-");
+  const [hour, setHour] = useState('-');
+  const [minute, setMinute] = useState('-');
+  const [second, setSecond] = useState('-');
 
-//competition date
-const date = Math.abs((new Date(2024, 2, 15).getTime() / 1000).toFixed(0));
+  //competition date
+  const date = Math.abs((new Date(2024, 2, 15).getTime() / 1000).toFixed(0));
 
-useEffect(() => {
+  useEffect(() => {
     function displayTimeLeft() {
-        const now = Math.abs((new Date().getTime() / 1000).toFixed(0));
-        const diff = date - now;
-      
-        const days = Math.floor(diff / 86400);
-        const hours = Math.floor(diff / 3600) % 24;
-        const minutes = Math.floor(diff / 60) % 60;
-        const seconds = diff % 60;
-        setDay(days)
-        setHour(hours)
-        setMinute(minutes)
-        setSecond(seconds)
-      }
+      const now = Math.abs((new Date().getTime() / 1000).toFixed(0));
+      const diff = date - now;
 
-      setInterval(() => { displayTimeLeft() }, 1000);
- 
-}, [])
+      const days = Math.floor(diff / 86400);
+      const hours = Math.floor(diff / 3600) % 24;
+      const minutes = Math.floor(diff / 60) % 60;
+      const seconds = diff % 60;
+      setDay(days);
+      setHour(hours);
+      setMinute(minutes);
+      setSecond(seconds);
+    }
+
+    setInterval(() => {
+      displayTimeLeft();
+    }, 1000);
+  }, []);
 
   return (
     <section
@@ -37,31 +38,31 @@ useEffect(() => {
       className="h-[100dvh] overflow-hidden relative flex justify-center items-center flex-col "
     >
       <Image
-        src="/images/mainMeshLeft.png"
+        src="/images/mainMesh.png"
         alt=""
         className="h-screen w-auto absolute top-0 left-0  hidden lg:block "
         width={1920}
         height={1000}
       />
       <Image
-        src="/images/mainMeshRight.png"
+        src="/images/mainMesh.png"
         alt=""
-        className="h-screen w-auto absolute top-0 right-0 hidden lg:block"
+        className="h-screen w-auto absolute top-0 right-0 hidden lg:block rotate-180"
         width={1920}
         height={1000}
       />
-      
+
       <Image
         src="/images/mainMeshMobile.png"
         alt=""
-        className="h-auto w-screen absolute top-0 right-0 block lg:hidden z-[-1]"
+        className="h-auto w-screen absolute top-0 md:top-[-30%]  right-0 block lg:hidden z-[-1]"
         width={1920}
         height={1000}
       />
       <Image
         src="/images/mainMeshMobile.png"
         alt=""
-        className="h-auto w-screen absolute bottom-0 rotate-180 right-0 block lg:hidden z-[1] opacity-90"
+        className="h-auto w-screen  absolute bottom-0 md:bottom-[-30%] rotate-180 right-0 block lg:hidden z-[1] opacity-90"
         width={1920}
         height={1000}
       />
@@ -69,12 +70,12 @@ useEffect(() => {
       <Image
         src="/images/mainsection.png"
         alt=""
-        className="mainSectionImage z-[-1] h-auto min-w-[400vw] lg:min-w-[100vw] absolute bottom-0 bg-gradient-to-b from-transparent to-Corange"
+        className="mainSectionImage z-[-1] h-auto min-w-[400vw] md:min-w-[200vw] lg:min-w-[150vw] xl:min-w-[110vw] absolute bottom-0 md:top-0 lg:bottom-auto "
         width={1920}
         height={1000}
       />
 
-      <h2 className="text-Cblue drop-shadow-md font-semibold uppercase tracking-[3px] lg:tracking-[4px] text-sm lg:text-lg">
+      <h2 className="text-Cblue drop-shadow-md text-center font-semibold uppercase tracking-[3px] lg:tracking-[4px] text-sm lg:text-lg">
         First ever hackathon of mlsa miet
       </h2>
       <h1 className="text-white uppercase tracking-widest text-5xl lg:text-7xl font-extrabold py-10 text-center ">
@@ -83,13 +84,13 @@ useEffect(() => {
       <h2 className="text-Cblue capitalize tracking-widest text-lg text-center ">
         32 Hours Offline Hackathon <br /> March 15 to March 17
       </h2>
-      <div className="space-x-5 space-y-5 w-[80vw] lg:w-auto mt-10 flex flex-col items-center lg:block ">
+      <div className="lg:space-x-5 space-y-5 w-[80vw] lg:w-auto mt-5 lg:mt-10 flex flex-col items-center lg:block ">
         <a
-      href={"#"}
-      className={`drop-shadow-lg font-semibold tracking-widest bg-Cblue px-8 lg:px-28 py-3 text-Corange rounded-full `}
-    >
-      Registrations Open Soon...
-    </a>
+          href={"#"}
+          className={`drop-shadow-lg text-nowrap font-semibold tracking-widest bg-Cblue px-8 lg:px-28 py-3 text-Corange rounded-full `}
+        >
+          Registrations Open Soon...
+        </a>
         <a
           href="#faq"
           className={`drop-shadow-lg font-semibold tracking-widest text-nowrap text-center px-28 lg:px-48 py-2 text-Cblue text-xl border-2 border-Cblue rounded-full  bg-gray-500 backdrop-blur-lg bg-opacity-10 `}
@@ -99,11 +100,22 @@ useEffect(() => {
       </div>
 
       {/* timer */}
-      <div className="timer space-x-5 absolute bottom-16 flex scale-90 ">
-        <div className="days"> {day}<div>days</div></div>
-        <div className="hours">{hour} <div>hours</div> </div>
-        <div className="minutes"> {minute} <div>minutes</div></div>
-        <div className="seconds">{second} <div>seconds</div></div>
+      <div className="timer space-x-5 absolute bottom-5 lg:bottom-16 flex scale-75 md:scale-90 lg:scale-100 ">
+        <div className="days">
+          {" "}
+          {day}
+          <div>days</div>
+        </div>
+        <div className="hours">
+          {hour} <div>hours</div>{" "}
+        </div>
+        <div className="minutes">
+          {" "}
+          {minute} <div>minutes</div>
+        </div>
+        <div className="seconds">
+          {second} <div>seconds</div>
+        </div>
       </div>
     </section>
   );
