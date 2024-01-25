f70ce57f45e90cc50ea0e9e485b40d7fa6c69571
Abhishek Singhal <86764280+Abhimrt@users.noreply.github.com>
2024-01-25 20:38:13 +0530

    ps update

--- app/problemstatement/page.js
+++ app/problemstatement/page.js
@@ -9,19 +9,93 @@ const data = [
         title: "",
         content: [
             {
+                psno: "PS01",
                 text: "How we can solve the problem of commuting from point A to B, through ev hoverboards, personal mobility robots and foldable e-scooter bots, while reducing traffic jams and pollution.",
                 desc: ""
             },
             {
+                psno: "PS02",
                 text: "Innovate any present application using AI/GenAI",
                 desc: ""
             },
             {
+                psno: "PS03",
                 text: "App to promote sustainable living",
                 desc: ""
             },
             {
+                psno: "PS04",
                 text: "AI based automation in DevOps",
+                desc: ""
+            },
+            {
+                psno: "PS05",
+                text: "It is difficult for organizers to engage with audience at tech events using online polling or quizzing apps due to multiple limitations which include limited audience, limited features or even user interface. How would you solve this? Design an efficient system for organizing engagements between participants with their mobile and laptops. Try to add as many creative options to engage beyond polls as well.",
+                desc: ""
+            },
+            {
+                psno: "PS06",
+                text: "Colleges have a mandate on the attendance of students and then a minimum of passing marks. But a lot of times students suffer when the right education or mentorship is not available. Design a system which can help students and college management to improve the quality of education and also help the best teaching professors to outshine.",
+                desc: ""
+            },
+            {
+                psno: "PS07",
+                text: "A lot of times we are attending events which are quite far away from our college. Upon reaching the event we find out that more people from our college have shown up. How could these people coordinate beforehand and pool together?",
+                desc: ""
+            },
+            {
+                psno: "PS08",
+                text: "Whenever you are purchasing a book, any appliance, laptop, or ordering food from outside, some people have zomato gold or swiggy premium subscriptions How to collectively order",
+                desc: ""
+            },
+            {
+                psno: "PS09",
+                text: "Often food mess has a fixed menu decided by the contractor. Sometimes the food isn't good, sometimes the menu isn't favorable to most of the students but we have to live with it. Can you build a system where people can collectively create a food menu by probably voting on options and also rate the meals after they have it. This can also be used to regulate the vendor and the money that is given to them.",
+                desc: ""
+            },
+            {
+                psno: "PS10",
+                text: "College infrastructure reporting system",
+                desc: ""
+            },
+            {
+                psno: "PS11",
+                text: "Mass bunk planner",
+                desc: ""
+            },
+            {
+                psno: "PS12",
+                text: "College students keep giving interviews and are selected or rejected at different times. But the questions which get asked don't get shared often. How can we build a system where people can post verified opportunities and also their experience of interviews or rating of how their internship went at a company",
+                desc: ""
+            },
+            {
+                psno: "PS13",
+                text: "Event poster designer",
+                desc: ""
+            },
+            {
+                psno: "PS14",
+                text: "Event poster generator like meme generator",
+                desc: ""
+            },
+            {
+                psno: "PS15",
+                text: "Event photo frame generator like meme generator",
+                desc: ""
+            },
+            {
+                psno: "PS16",
+                text: "Intra college event tracker for students",
+                desc: ""
+            },
+            {
+                psno: "PS17",
+                text: "Event photo sharing application which can automatically tag and group all your photos once you mark yourself",
+                desc: ""
+            },
+            {
+                psno: "PS18",
+                text: "Watch party for watching a movie or a series together",
                 desc: ""
             },
         ]
@@ -95,9 +169,9 @@ const ProblemContainer = ({ problemData, toggle }) => {
                     <div className='my-10' key={i}>
                         <Heading text={e.title} />
                         <table className='w-[90%] lg:w-[80%] mx-auto pstable shadow-lg my-5 text-xl' >
-                            <thead className='text-lg font-medium'>
+                            <thead className='text-[1px] sm:text-lg font-medium'>
                                 <tr>
-                                    <td>S.No.</td>
+                                    <td className='text-nowrap'>PS. No.</td>
                                     <td>Problem Statement</td>
                                 </tr>
                             </thead>
@@ -105,7 +179,7 @@ const ProblemContainer = ({ problemData, toggle }) => {
                                 {
                                     e.content.map((e1, i1) => (
                                         <tr key={i1} onClick={() => toggle(i, i1)}>
-                                            <td>{i1 + 1}</td>
+                                            <td>{e1.psno}</td>
                                             <td>{e1.text} {e1.desc != "" && <span className='cursor-pointer text-blue-700'>Read More..</span>}</td>
                                         </tr>
                                     ))
