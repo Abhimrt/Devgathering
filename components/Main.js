3f3a0917f9bcaf19adf2e5ce68895be3f948cede
Yabhi1205 <83153764+yabhi1205@users.noreply.github.com>
2024-02-11 10:19:12 +0000

    adding the button

--- components/Main.js
+++ components/Main.js
@@ -28,9 +28,18 @@ const Main = () => {
       setSecond(seconds);
     }
 
+
     setInterval(() => {
       displayTimeLeft();
     }, 1000);
+    const script = document.createElement('script');
+    script.src = 'https://apply.devfolio.co/v2/sdk.js';
+    script.async = true;
+    script.defer = true;
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    }
   }, []);
 
   return (
@@ -99,6 +108,22 @@ const Main = () => {
         >
           See FAQs
         </Link>
+
+
+
+        <div className=" w-full flex justify-center ">
+
+          <div
+            class="apply-button"
+            data-hackathon-slug="mlsamiet-devgathering"
+            data-button-theme="light"
+            style={{ height: "44px", width: "312px" }}
+          >
+          </div>
+        </div>
+
+
+
       </div>
 
       {/* timer */}
