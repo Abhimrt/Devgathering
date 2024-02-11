313250a10a8cf547ea43b46fe779479ebd718116
Yabhi1205 <83153764+yabhi1205@users.noreply.github.com>
2024-02-11 07:36:18 +0000

    adding the devfolio

--- components/Sponsors.js
+++ components/Sponsors.js
@@ -11,9 +11,18 @@ const Sponsors = () => {
       </h2>
       <p className='text-Ccream text-lg text-center mx-5'>Want to be featured here and be a part of this event and help us. Fill the form given form below</p>
       <a href="https://abhisheksinghal.notion.site/abhisheksinghal/MLSA-MIET-Dev-Gathering-Call-for-Sponsors-0005e944a676484d96b298479af82617" target='_blank' className='text-Ccream border-2 border-Ccream px-5 py-2 rounded-xl mt-2 mb-5 hover:bg-Cblue duration-300 text-nowrap' >Sponsor Us</a>
-      <h3 className=' text-2xl rounded-full pb-2 pt-3 px-10 shadow-sm bg-Cblue text-Corange'>In Kind</h3>
       <div className='w-screen overflow-hidden  min-h-[40vh] flex justify-center items-center'>
-        <div className='sponsorScroll  overflow-x-auto  flex justify-center items-center flex-wrap px-12 lg:pl-24 sm:gap-10  lg:gap-14'>
+        <div className='sponsorScroll  overflow-x-auto  flex justify-center items-center flex-wrap px-12 lg:pl-24 gap-10  lg:gap-14'>
+          {/* <h3 className='text-Ccream text-center text-3xl w-screen ' >Declare soon...</h3> */}
+          <Box1 image="sponsorsDevfolio" name="DEVFOLIO" />
+          <Box1 image="sponsorsPolygon" name="PLOYGON" />
+          <Box1 image="sponsorsETH" name="ETHINDIA" />
+
+        </div>
+      </div>
+      <h3 className=' text-2xl rounded-full pb-2 pt-3 mt-8 px-10 shadow-sm bg-Cblue text-Corange'>In Kind</h3>
+      <div className='w-screen overflow-hidden  min-h-[40vh] flex justify-center items-center'>
+        <div className='sponsorScroll  overflow-x-auto  flex justify-center items-center flex-wrap px-12 lg:pl-24 sm:gap-10  lg:gap-20'>
           {/* <h3 className='text-Ccream text-center text-3xl w-screen ' >Declare soon...</h3> */}
           <Box image="sponsorsXYZ" name=".XYZ" />
           <Box image="sponsorsECHO" name="Echo3d" />
@@ -29,12 +38,23 @@ export default Sponsors
 
 const Box = ({ image, name }) => {
   return (
-    <div className=' w-auto space-y-8 flex justify-center items-center flex-col h-[40vh]' >
-      <div className='h-[22vh] aspect-square shadow-2xl rounded-xl overflow-hidden rotate-12 hover:rotate-0 duration-150 grid place-content-center relative ' >
-        <Image src={`/images/${image}.png`} className='w-[80%] opacity-20 animate-ping-slow mx-auto absolute -rotate-12 hover:rotate-0 duration-300' width={400} height={400} alt="" />
-        <Image src={`/images/${image}.png`} className='w-[80%]  mx-auto -rotate-12 hover:rotate-0 duration-300' width={400} height={400} alt="" />
+    <div className=' w-auto space-y-8 flex justify-center items-center flex-col h-[50vh]' >
+      <div className='h-[30vh] aspect-square shadow-2xl rounded-xl overflow-hidden rotate-12 hover:rotate-0 duration-150 grid place-content-center relative ' >
+        <Image src={`/images/${image}.png`} className='w-[80%] opacity-20 animate-ping-slow mx-auto absolute -rotate-12 hover:rotate-0 duration-300' width={400} height={400} alt={`${name} LOGO`} />
+        <Image src={`/images/${image}.png`} className='w-[80%]  mx-auto -rotate-12 hover:rotate-0 duration-300' width={400} height={400} alt={`${name} LOGO`} />
       </div>
       <h3 className='h-[5vh] w-full shadow-xl grid place-content-center rounded-xl text-xl' >{name}</h3>
+    </div>
+  )
+}
+const Box1 = ({ image, name }) => {
+  return (
+    <div className=' w-auto space-y-8 flex justify-center items-center flex-col ' >
+      <div className=' overflow-hidden duration-150 grid place-content-center relative ' >
+        {/* <Image src={`/images/${image}.png`} className='w-[90%] opacity-20 animate-ping-slow mx-auto absolute -rotate-12 duration-300' width={400} height={400} alt={`${name} LOGO`} /> */}
+        <Image src={`/images/${image}.png`} className=' shadow-md  mx-auto duration-300' width={300} height={400} alt={`${name} LOGO`} />
+      </div>
+      {/* <h3 className='h-[5vh] w-full shadow-xl grid place-content-center rounded-xl text-xl' >{name}</h3> */}
     </div>
   )
 }
