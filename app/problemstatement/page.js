8f739cdf00fe840ad65a6e0b589ca18bf1f93f16
yabhi2512 <160402976+yabhi2512@users.noreply.github.com>
2024-02-29 07:03:45 +0000

    adding the teachers

--- app/problemstatement/page.js
+++ app/problemstatement/page.js
@@ -189,7 +189,7 @@ const page = () => {
             <section className='flex justify-center items-center flex-col my-10 mt-28 lg:mt-40'>
                 <h1 className='cursor-pointer  text-3xl lg:text-4xl font-semibold tracking-widest'>{"{"}Problem
                     <span className=' cursor-pointer bg-Cblue text-Corange px-1 py-2'>Statement{"}"}</span>
-                    <button onFocus={handleFocus} onBlur={handleBlur} className='h-full  ' data-popover-target="popover-description" data-popover-placement="bottom-end" type="button">
+                    <button onFocus={handleFocus} onBlur={handleBlur} className='h-full ' data-popover-target="popover-description" data-popover-placement="bottom-end" type="button">
                         <svg class="w-10 h-10 my-auto ms-2 text-gray-400 hover:text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd">
                             </path>
@@ -214,7 +214,7 @@ const page = () => {
                 </h1>
                 <div className='mt-10 lg:mt-16 mx-0 w-screen'>
 
-                    <Heading text={"Tracks"} />
+                    <Heading text={"Themes"} />
                     <div className=' flex flex-wrap xl:flex-row text-center capitalize max-lg:flex-col lg:justify-around mt-8 max-w-screen lg:mx-12'>
                         {
                             tracks.map((e, i) => {
