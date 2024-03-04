7fe5c421930aeb66de2088d244bbaa7e324bcd74
yabhi2512 <160402976+yabhi2512@users.noreply.github.com>
2024-03-04 15:13:30 +0000

    adding the sponsors

--- components/Data.js
+++ components/Data.js
@@ -1,21 +1,29 @@
 export const FaqsData = [
-    {
-      Question: "I’ve submitted wrong details in the registration form, how can I correct them?",
-      Answer: "All corrections can be handled at the time of physical registrations, nothing needs to be done as of now.",
+  {
+    Question: "Do I need to submit an application?",
+    Answer: "Yes, submitting your application for review is mandatory. We won't be able to review your project without a proper application.",
     },
-    {
-      Question: "I cannot attend the event for some reason, can someone attend it in my place?",
-      Answer: "Yes, as long as the following conditions are met: The replacement attendee meets the eligibility criteria. A mail has been written to VANI.MITTAL@studentambassadors.com in before the event, stating the change.",
+  {
+    Question: "I’ve submitted wrong details in the registration form, how can I correct them?",
+    Answer: "So, you have to re-register from a new id.",
     },
-    {
-      Question: "Can I cancel my registration?",
-      Answer: "Cancellations will be handled on a case-to-case basis. Drop an email to VANI.MITTAL@studentambassadors.com in stating your reason for cancellation.",
+  {
+    Question: "Can I cancel my registration?",
+    Answer: "Cancellations will be handled on a case-to-case basis. Drop an email to VANI.MITTAL@studentambassadors.com in stating your reason for cancellation.",
     },
-    {
-      Question: "Do I need to pay money to register for the Hackathon?",
-      Answer: "No, it's completely free!",
+  {
+    Question: "Can the participants of a team be from different colleges?",
+    Answer: "Yes. There is no restriction here.",
     },
-    {
+  {
+    Question: "Do I have to choose a pre-defined problem statement?",
+    Answer: "No, you don't! While we offer various tracks with specific challenges (problem statements), participation in them is optional. You can choose a track and pursue your own innovative idea related to its theme, which is called open innovation.",
+  },
+  {
+    Question: "Do I need to pay money to register for the Hackathon?",
+    Answer: "No, it's completely free!",
+    },
+  {
       Question: "Do I need to be in Meerut to take part in the Hackathon?",
       Answer: "You can apply, if you live in India but you have to be present onsite in MIET, Meerut on the day of hackathon.",
     },
@@ -23,54 +31,54 @@ export const FaqsData = [
       Question: "What is the AI usage policy?",
       Answer: "AI tools are allowed. However, they must explicily mentioned wherever used in the codebase and cited in the final presentation.",
     },
-    {
-      Question: "Do I need to have any specific qualification to be a participant for the Hackathon?",
-      Answer: "Yes, you must be a student from any University/College in India.",
-    },
-    {
-      Question: "Do I need to be a student?",
-      Answer: "Yes, you must be a student from any University/College in India.",
-    },
-    {
-      Question: "What kind of solution will be accepted?",
-      Answer: "Both technology based (software) as well as non-tech (hardware) based solutions will be accepted.",
-    },
-    {
-      Question: "Is it an individual or team event?",
-      Answer: "Students cannot participate individually. The maximum strength of a team can be 5 members and The minimum strength of a team can be 3 members. Every team member needs to fulfil the eligibility criteria. Teams must be formed before registration.",
-    },
-    {
-      Question: "Can we form our own teams, or will we be allotted to a particular team?",
-      Answer: "Participants are free to form their own teams. However, you must form your team before registration.",
-    },
-    {
-      Question: "Can a college have multiple teams?",
-      Answer: "Yes. There is no restriction on the number of teams from a college.",
-    },
-    {
-      Question: "Can the participants of a team be from different colleges?",
-      Answer: "Yes. There is no restriction here.",
-    },
-    {
-      Question: "How detailed should be the presentation?",
-      Answer: "You will be asked to present your idea to the judges in-person. Please work accordingly.",
-    },
-    {
-      Question: "What is prototype code?",
-      Answer: "It is basically a working code of the solution you are providing. It might not be fully prepared with all the functionalities, maybe just with the few main ones.",
-    },
-    {
-      Question: "Since there is no specific technology mentioned, are there any restrictions on using number of pre-built libraries?",
-      Answer: "There is no restriction to use any language, technology stack, or libraries. You can use any of them to create the web/mobile application. However, if you wish to use AI tools you must explicitly mention them in your presentation and submission.",
-    },
-    {
-      Question: "Who will own the IP(Intellectual Property) Rights to the product that I have built?",
-      Answer: "For the most part, the developer/developers of the web/mobile application will have all rights and own the IP of the product, unless specified otherwise by the problem statement. However, all code needs to be in public domain (open source) so that it can be evaluated by the judges.",
+  {
+    Question: "Do I need to be a student?",
+    Answer: "Yes, you must be a student from any University/College in India.",
     },
   {
-      Question: "Where can I get to know about the event details?",
-      Answer: "All details regarding the event are available on this website. Announcements will be made on through email.",
+    Question: "What kind of solution will be accepted?",
+    Answer: "Both technology based (software) as well as non-tech (hardware) based solutions will be accepted.",
     },
+  {
+    Question: "Is it an individual or team event?",
+    Answer: "Students cannot participate individually. The maximum strength of a team can be 5 members and The minimum strength of a team can be 3 members. Every team member needs to fulfil the eligibility criteria. Teams must be formed before registration.",
+    },
+  {
+    Question: "Can we form our own teams, or will we be allotted to a particular team?",
+    Answer: "Participants are free to form their own teams. However, you must form your team before registration.",
+    },
+  {
+    Question: "Can a college have multiple teams?",
+    Answer: "Yes. There is no restriction on the number of teams from a college.",
+    },
+  {
+    Question: "How detailed should be the presentation?",
+    Answer: "You will be asked to present your idea to the judges in-person. Please work accordingly.",
+    },
+  {
+    Question: "I cannot attend the event for some reason, can someone attend it in my place?",
+    Answer: "Yes, as long as the following conditions are met: The replacement attendee meets the eligibility criteria. A mail has been written to VANI.MITTAL@studentambassadors.com in before the event, stating the change.",
+  },
+  {
+    Question: "What is prototype code?",
+    Answer: "It is basically a working code of the solution you are providing. It might not be fully prepared with all the functionalities, maybe just with the few main ones.",
+    },
+  {
+    Question: "Since there is no specific technology mentioned, are there any restrictions on using number of pre-built libraries?",
+    Answer: "There is no restriction to use any language, technology stack, or libraries. You can use any of them to create the web/mobile application. However, if you wish to use AI tools you must explicitly mention them in your presentation and submission.",
+    },
+  {
+    Question: "Who will own the IP(Intellectual Property) Rights to the product that I have built?",
+    Answer: "For the most part, the developer/developers of the web/mobile application will have all rights and own the IP of the product, unless specified otherwise by the problem statement. However, all code needs to be in public domain (open source) so that it can be evaluated by the judges.",
+    },
+  {
+    Question: "Where can I get to know about the event details?",
+    Answer: "All details regarding the event are available on this website. Announcements will be made on through email.",
+    },
+  {
+    Question: "Do I need to have any specific qualification to be a participant for the Hackathon?",
+    Answer: "Yes, you must be a student from any University/College in India.",
+  },
   {
       Question: "Whom can I contact for queries?",
       Answer: "For any questions or queries, drop us an email at VANI.MITTAL@studentambassadors.com",
